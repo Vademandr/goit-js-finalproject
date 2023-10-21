4c5bd56bdd0a01dfac15f7b48e70ac8218b4d3f4
Vademandr <140320640+Vademandr@users.noreply.github.com>
2023-10-21 13:13:56 +0300

    create2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryElements = document.querySelector('.gallery');
+
+function createGalleryElement({ original, preview, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+}
+
+function selectGallery(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+
+  const instance = basicLightbox.create(
+    `<img class="gallery__image-big" src="${event.target.dataset.source}">`,
+    {
+      onShow: () => window.addEventListener('keydown', onEsc),
+      onClose: () => window.removeEventListener('keydown', onEsc),
+    },
+  );
+
+  const onEsc = event => {
+    if (event.code === 'Escape') instance.close();
+  };
+
+  instance.show();
+}
+
+const galleryElementSet = galleryItems.map(createGalleryElement).join('');
+
+galleryElements.insertAdjacentHTML('beforeend', galleryElementSet);
+galleryElements.addEventListener('click', selectGallery);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryElements = document.querySelector('.gallery');
+
+function createGalleryElement({ original, preview, description }) {
+  return `
+    <li class="gallery__item" id="item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>
+    </li>
+  `;
+}
+
+function createGallery(galleryItems) {
+  return galleryItems.map(createGalleryElement).join('');
+}
+
+const galleryElementSet = createGallery(galleryItems);
+
+galleryElements.insertAdjacentHTML('beforeend', galleryElementSet);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
